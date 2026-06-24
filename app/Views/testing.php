c57e1015b6f988b641e526c4bc70acb966db7618
Ratinan Lee <lee.ratinan@gmail.com>
2026-06-24 13:51:46 +0800

    update testers

--- app/Views/testing.php
+++ app/Views/testing.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API Endpoint Tester</title>
+    <title>OtterQR - TESTER</title>
+    <!-- Favicons -->
+    <link href="https://otternova.com/assets/img/favicon.webp" rel="icon">
+    <link href="https://otternova.com/assets/img/apple-touch-icon.webp" rel="apple-touch-icon">
+    <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { padding: 20px; background-color: #f8f9fa; }
